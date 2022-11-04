18a08994af92e3920476809ee25eae284a7d20b4
Ammar Kasem (Lexicon) <linkamar@gmail.com>
2022-11-04 15:38:20 +0100

    part 2

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_cli.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+    }
+}
